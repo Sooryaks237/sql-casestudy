d67550fe7d407885ab47f3c755fe7a988bdd5906
Soorya K S <sooryaks552@gmail.com>
2025-09-30 13:04:35 +0530

    Update 01_schema.sql

--- 01_schema.sql
+++ 01_schema.sql
@@ -152,129 +152,54 @@ SELECT * FROM `productlines` LIMIT 0, 5;
 
 -- Questions:
 
--- Q-6: Insert the following two rows in the 'customers' table.
+-- Q-1: Insert the following two rows in the 'customers' table.
 -- (495,'Diecast Collectables','Franco','Valarie','Boston','MA','51003','USA','1188',85100),
 -- (496,'Kelly\'s Gift Shop','Snowden','Tony','Auckland  ','NULL','NULL','New Zealand','1612',110000)
-insert into customers values
-(495,'Diecast Collectables','Franco','Valarie','Boston','MA','51003','USA','1188',85100),
-(496,'Kelly\'s Gift Shop','Snowden','Tony','Auckland  ','NULL','NULL','New Zealand','1612',110000);
 
--- Q-7: In the "employees" table there are some entries where 'SR' is written instead of 'Sales Rep' where office code is equal to 4.
+-- Q-2: In the "employees" table there are some entries where 'SR' is written instead of 'Sales Rep' where office code is equal to 4.
 -- Update the 'employees' table by inserting a job title as 'Sales Rep' where office code is equal to 4.
-update employees 
-set jobTitle='Sales Rep' where officeCode=4;
 
--- Insert the following entry into the employee table.
-insert into employees 
-values
-(1102, 'Bondur', 'Gerard', 'x5408', 'gbondur@classicmodelcars.com', 4, '1056', 'Sale Manager(EMEA)');
+-- Q-3: Insert the following entry into the employee table.
+-- (1102, 'Bondur', 'Gerard', 'x5408', 'gbondur@classicmodelcars.com', 4, '1056', 'Sale Manager(EMEA)')
 
--- Q-8: There is no product under category of boat. Hence, delete the Boat entry from productlines table.
-delete from productlines where productLine='Boats';
-	
-select * from employees;
+-- Q-4: There is no product under category of boat. Hence, delete the Boat entry from productlines table.
 
-select * from productlines;
+-- Q-5: Convert the 'quantityOrdered' column's data type into int from varchar.
 
--- Q-9: Convert the 'quantityOrdered' column's data type into int from varchar.
-alter table orderdetails 
-modify column quantityOrdered int;
-
-describe orderdetails;
-
--- Q-10: Print the employees with the job title “Sales Rep”. 
+-- Q-6: Print the employees with the job title “Sales Rep”. 
 -- What is the first name of the employee that appears on the top after applying this query?
-select firstName from employees where jobTitle='Sales Rep';
 
--- Q-10: Find the total number of employees from the 'employees’ table and alias it as "Total_Employees".
-select count(*) as Total_Employees from employees;
+-- Q-7: Find the total number of employees from the 'employees’ table and alias it as "Total_Employees".
 
--- Q-10: How many customers belongs to Australia? also alias it as "Australia_Customers".
-select count(*) as Australia_Customers from customers where country='Australia';
+-- Q-8: How many customers belongs to Australia? also alias it as "Australia_Customers".
 
--- Q-11: Print the quantity in stock for "Red Start Diecast" product vendors with product line is "Vintage Cars" from the table "products".
-select sum(quantityInStock) from products
-where productVendor='Red Start Diecast' and productLine='Vintage Cars';
+-- Q-9: Print the quantity in stock for "Red Start Diecast" product vendors with product line is "Vintage Cars" from the table "products".
 
--- Q-11: Count the total number of orders that has not been shipped yet in the "orders" table. 
-select count(*) from orders where status!='Shipped';
+-- Q-10: Count the total number of orders that has not been shipped yet in the "orders" table. 
 
--- Q-12: Count the entries in "orderdetails" table with "productCode" starts with S18 and "priceEach" greater than 150.
-select count(*) from orderdetails
-where productCode like 'S18%' and priceEach>150;
+-- Q-11: Count the entries in "orderdetails" table with "productCode" starts with S18 and "priceEach" greater than 150.
 
--- Q- 13: What are the top three countries which have the maximum number of customers?
-select country, count(customerNumber) as customer_count from customers
-group by 1 order by 2 desc
-limit 3;
+-- Q- 12: What are the top three countries which have the maximum number of customers?
 
--- Q-14: What is the average credit limit for Singapore from "customers" table?
-select country, avg(creditLimit) as credit_limit from customers
-where country='Singapore';
+-- Q-13: What is the average credit limit for Singapore from "customers" table?
 
--- Q-15: What is the total amount to be paid by the customer named as “Euro+ Shopping Channel”?
+-- Q-14: What is the total amount to be paid by the customer named as “Euro+ Shopping Channel”?
 -- You need to use the “customers” and “payments” tables to answer this question.
-select customerNumber, customerName from customers
-where customerName='Euro+ Shopping Channel';
 
-select customerNumber, sum(amount) as total_amount from payments
-where customerNumber=141;
-
--- Q-16: Which month has recieved the maximum aggragated payments from the customers? 
+-- Q-15: Which month has recieved the maximum aggragated payments from the customers? 
 -- Q-16: What is the aggregated value of the payment recieved from that month?
-select month(paymentDate) as paymentmonth, sum(amount) as totalamount from payments 
-group by 1 order by 2 desc limit 1;
 
 -- Q-17: What is the shipped date of the maximum quantity ordered for "1968 Ford Mustang" product name?
-select a.shippedDate 
-from orders a
-where a.orderNumber=
-	(select b.orderNumber 
-    from orderdetails b join products c 
-    on b.productCode=c.productCode
-    where c.productName='1968 Ford Mustang'
-    group by b.orderNumber
-    order by sum(b.quantityOrdered) desc
-    limit 1);
 
 -- Q-18: Inner join:  What is the average value of credit limit corresponds to the customers which have been contacted by the employees with their office located in “Tokyo” city? 
-select avg(a.creditLimit)
-from customers a inner join employees b
-on a.salesRepEmployeeNumber=b.employeeNumber
-where officeCode=
-	(select officeCode 
-    from offices
-    where office_city='Tokyo');
 
 -- Q-19: Outer Join: What is the name of the customer which has paid the lowest amount to the company. 
-select CustomerName, sum(amount) as totalamount
-from payments a left join customers b
-on a.customerNumber=b.customerNumber
-group by 1
-order by 2
-limit 1;
 
 -- Q-20: Outer Join: What is the city of the employee whose job title is "VP Marketing" ? 
-select office_city, jobTitle
-from employees a left join offices b
-on a.officeCode=b.officeCode
-where jobTitle='VP Marketing';
 
 -- Q-21: What is the name of the customer who belongs to ‘France’ and has the maximum creditLimit among the customers in France?
-select customerName, country
-from customers
-where creditLimit=(select max(creditLimit) from customers where country='France'
-group by country);
 
 -- Q-22: What will be the remaining stock of the product code that equals ‘S18_1589’ if it is sent to all the customers who have demanded it?
-select quantityInStock from products where productCode='S18_1589';
-
-select sum(quantityOrdered) from orderdetails where productCode='S18_1589';
-
--- Ans: 9042-350=8692
 
 -- Q-23: What is the average amount paid by the customer 'Mini Gifts Distributors Ltd.'?
-select customerName,avg(amount)
-from customers a inner join payments b
-on a.customerNumber=b.customerNumber
-where customerName='Mini Gifts Distributors Ltd.';
+
